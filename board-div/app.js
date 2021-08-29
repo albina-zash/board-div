64ecbda789cf8a93e67d764c4a08c04a1cce8528
Albina Zashchyrinska <65358105+zelobin@users.noreply.github.com>
2021-08-29 10:24:44 +0300

    Add files via upload

---
+++ board-div/app.js
@@ -0,0 +1,32 @@
+const  board = document.querySelector('#board')
+const colors = ['#e31b05', '#00c9af','#04a4de', '#578732', '#dfe305', '#6205e3', '#e31b05', '#6205e3']
+const  SQUARES_NUMBER = 945
+
+for (let i=0; i<SQUARES_NUMBER; i++){
+    const  square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', setColor)
+
+    square.addEventListener('mouseleave', removeColor)
+
+    board.append(square)
+}
+
+function setColor (event) {
+    const element = event.target
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor (event) {
+    const element = event.target
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
